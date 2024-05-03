3228f7b42d64cccdc8a0cb62c3581aae24ac1598
Phoenix0415 <phoenixx0415@gmail.com>
2024-05-03 14:34:12 +0930

    add schemas in the ./backend/schemaTypes

---
+++ backend/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import {userType} from './userType.js'
+import {pinType} from './pinType.js'
+import {saveType} from './saveType.js'
+import {commentType} from './commentType.js'
+import {postedByType} from './postedByType.js'
+
+export const schemaTypes = [userType, pinType, saveType, commentType, postedByType]
