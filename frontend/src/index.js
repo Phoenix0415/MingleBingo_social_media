a1e46e5894ba0807a62e8c4966e39b50045885ac
Phoenix0415 <phoenixx0415@gmail.com>
2024-05-05 15:18:29 +0930

    change frontend to multi-page structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { unstable_createRoot } from "react-dom"; // Importing unstable_createRoot from react-dom
+import React from 'react';
+import { createRoot } from 'react-dom';
 
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
-// Use unstable_createRoot instead of ReactDOM.render
-const rootElement = unstable_createRoot(root);
+const rootElement = createRoot(root);
 rootElement.render(<App />);
